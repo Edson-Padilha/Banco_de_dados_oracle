10bff7c9d8c656324e141f2caf6bbcb6a815b2e3
Edson Marciano Rodrigues Padilha <b12206@bra01ad01.local>
2022-04-23 22:48:25 -0300

    Clausula WHERE

---
+++ 5_where.py
@@ -0,0 +1,18 @@
+import sqlite3
+import random
+
+con = sqlite3.connect('Brandili.db')
+
+Cursor = con.cursor() 
+Cursor.execute('INSERT INTO Minha_Tabela VALUES ("14/01/2020","Isa","02")')
+
+for loop in range(10):
+    numero = random.randint(50,80)
+    Cursor.execute(f'INSERT INTO Minha_Tabela VALUES ("17/11/1953", "Silvestre",{numero})')
+
+consulta = Cursor.execute('''SELECT nome, idade FROM Minha_Tabela 
+WHERE idade > 70''').fetchall()
+
+for linhas in consulta:
+
+    print(linhas)
